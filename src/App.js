e967682fe36f5ad953740c66190e2f6f5a7daa78
Rohan Gupta <56235204+rohan9896@users.noreply.github.com>
2021-03-10 12:49:12 +0530

    changed folder structure

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import './Components/Buttons'
 import Buttons from './Components/Buttons';
+import Avatar from './Components/Avatar'
 
 function App() {
   return (
     <div className="App">
       <Buttons />
+      <Avatar />
     </div>
   );
 }
--- src/Components/Avatar.js
+++ src/Components/Avatar.js
@@ -1,10 +1,12 @@
 import React from 'react'
+import '../base.css';
+// import 'Avatar.css'
 
 function Avatar() {
     return (
         <div>
             <h2>Avatars</h2>
-            
+            <img className="avatar" src="https://instagram.fixc1-4.fna.fbcdn.net/v/t51.2885-19/s320x320/156517218_440161457404541_6372279708468127589_n.jpg?tp=1&_nc_ht=instagram.fixc1-4.fna.fbcdn.net&_nc_ohc=c6EPoG91QdcAX9bQ5BL&oh=e55f0a90c9f1b2276e37c58688947703&oe=60707CB6" alt="me" />
         </div>
     )
 }
