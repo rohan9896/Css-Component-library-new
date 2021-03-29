21b89dbc7da2a730753a9fd1b7628c4fadd90440
Rohan Gupta <56235204+rohan9896@users.noreply.github.com>
2021-03-30 01:27:41 +0530

    added sidebar and moved state of selected component in context

--- src/Components/MobileList.js
+++ src/Components/MobileList.js
@@ -1,38 +1,38 @@
 import React, { useState } from "react";
+import { useSelectedComponent } from "./Context/selected-component-context";
+import "./MobileList.css";
 import Buttons from "./Buttons";
 import Alert from "./Alert";
 import Navbar from "./Navbar";
 import Avatar from "./Avatar";
 import Input from "./Input";
 import Card from "./Card";
-import "./ComponentsList.css";
 import Icons from "./Icons";
 
 const components = [
-  { name: "Buttons", id: 1, component: <Buttons /> },
-  { name: "Icons", id: 2, component: <Icons /> },
-  { name: "Avatar", id: 3, component: <Avatar /> },
-  { name: "Input", id: 4, component: <Input /> },
-  { name: "Navbar", id: 5, component: <Navbar /> },
-  { name: "Alert", id: 6, component: <Alert /> },
-  { name: "Card", id: 7, component: <Card /> },
-];
+    { name: "Buttons", id: 1, component: <Buttons /> },
+    { name: "Icons", id: 2, component: <Icons /> },
+    { name: "Avatar", id: 3, component: <Avatar /> },
+    { name: "Input", id: 4, component: <Input /> },
+    { name: "Navbar", id: 5, component: <Navbar /> },
+    { name: "Alert", id: 6, component: <Alert /> },
+    { name: "Card", id: 7, component: <Card /> },
+  ];
 
-function ComponentsList() {
-  const [selectedComp, setSelectedComp] = useState(components[0]);
+function MobileList() {
   const [hasPopup, setHasPopup] = useState(false);
+  const { selectedComp, setSelectedComp } = useSelectedComponent();
 
   return (
-    <div className="components__page">
-      <div className="Mobile__list">
-        <div className="dropdown" onClick={() => setHasPopup(!hasPopup)}>
-          <button>
+      <div className="MobileList">
+        <div className="dropdown">
+          <button onClick={() => setHasPopup(!hasPopup)}>
             <span>{selectedComp.name}</span>&nbsp;&nbsp;&nbsp;&nbsp;
             <span>
               <img
                 style={{ height: "1rem" }}
                 src="https://raw.githubusercontent.com/rohan9896/Testing-for-CSS-component-library/e3d92bc61c070bf395331cd48c325b983b95e92c/icons/down-arrow.svg"
-                alt="arrow"
+                alt="down arrow"
               />
             </span>
           </button>
@@ -53,10 +53,7 @@ function ComponentsList() {
           </ul>
         </div>
       </div>
-      <div className='components'>{selectedComp.component}</div>
-    </div>
   );
 }
 
-export default ComponentsList;
-
+export default MobileList;
