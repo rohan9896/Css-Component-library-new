2b23c28a4b72853e5317fcfcedeab705a29fea52
Rohan Gupta <56235204+rohan9896@users.noreply.github.com>
2021-04-04 01:37:41 +0530

    added card2

--- src/Components/Card.js
+++ src/Components/Card.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import "../base.css";
 import "./Card.css";
 import CodeContainer from "./CodeContainer";
@@ -6,7 +6,10 @@ import Description from "./Description";
 import "./Heading.css";
 
 function Card() {
-  const copyCode = `
+
+  const [selected, setSelected] = useState('card')
+
+  const card = `
   <div class="card">
   <img
     alt="model"
@@ -31,15 +34,32 @@ function Card() {
 </div>
   `;
 
+  const card2 = `
+  <div class="card2">
+    <img alt="prod" src="https://github.com/rohan9896/Testing-for-CSS-component-library/blob/main/icons/card%20component/prod2.jpeg?raw=true" />
+    <span className="card2__brand">Adidas</span>
+    <span>Flat 80% OFF</span>
+  </div>
+  `
+
+  let copyCode;
+  switch (selected) {
+    case "card":
+      copyCode = card;
+      break;
+    case "card2":
+      copyCode = card2;
+      break;
+    default:
+      break;
+  }
+
   return (
     <div className="Card__container">
       <h1 className="title">Cards</h1>
       <Description desc="Card is used to display information about any person/product/etc." />
-      <div className="card">
-        <img
-          alt="model"
-          src="https://bit.ly/2NY8Tx4"
-        ></img>
+      <div className="card" onClick={() => setSelected("card")}>
+        <img alt="model" src="https://bit.ly/2NY8Tx4"></img>
         <div className="productInfo">
           <p>Product Name</p>
           <small>A short description</small>
@@ -54,9 +74,14 @@ function Card() {
             class="ecomm__icon"
             src="https://bit.ly/3rtgodg"
             alt="white heart"
-            style={{height: "2rem", width: "2rem"}}
+            style={{ height: "2rem", width: "2rem" }}
           />
         </span>
+      </div>
+      <div className="card2" onClick={() => setSelected("card2")}>
+        <img alt="prod" src="https://github.com/rohan9896/Testing-for-CSS-component-library/blob/main/icons/card%20component/prod2.jpeg?raw=true" />
+        <span className="card2__brand">Adidas</span>
+        <span>Flat 80% OFF</span>
       </div>
       <CodeContainer codeForCopy={copyCode} />
     </div>
