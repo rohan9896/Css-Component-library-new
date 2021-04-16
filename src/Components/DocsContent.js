0caf20f3edcc3c620cb32e9b5b3f6879ebd71881
Rohan Gupta <56235204+rohan9896@users.noreply.github.com>
2021-04-16 12:59:03 +0530

    Update DocsContent.js

--- src/Components/DocsContent.js
+++ src/Components/DocsContent.js
@@ -4,6 +4,18 @@ import '../base.css'
 
 const copyCode = `@import url("https://effortlessui.netlify.app/base.css");`;
 
+const reactComponentsContainerStyle = {
+  display: "flex",
+  flexDirection: "column",
+  borderTop: "1px solid rgb(216, 207, 207)"
+}
+
+const linkStyle= {
+  color: "red",
+  textDecoration: "none",
+  margin: "1rem"
+}
+
 function DocsContent() {
   return (
     <div>
@@ -13,6 +25,11 @@ function DocsContent() {
           <CodeContainer codeForCopy={copyCode} />
           <h4>OR Download the CSS file by clicking below</h4>
           <a href="https://effortlessui.netlify.app/base.css" download='base.css'><button style={{cursor: "pointer"}} className="primary-button  blue">Download</button></a>
+      </div>
+      <div style={reactComponentsContainerStyle}>
+        <h1>Also, checkOut React Components-</h1>
+        <a style={linkStyle} href="https://codesandbox.io/dashboard/all/ecommerce-components?workspace=6d5fb724-6080-4712-8316-96a5bcb2a71b">E-Commerce Website Components</a>
+        <a style={linkStyle} href="https://codesandbox.io/dashboard/all/video-lib?workspace=6d5fb724-6080-4712-8316-96a5bcb2a71b">Video Library(Youtube like app) Components</a>
       </div>
     </div>
   );
